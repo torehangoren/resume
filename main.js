81f20f014bf6fc55c19f3b5e0c7f0231d2d220ca
Törehan Gören <goto@tgw-group.com>
2024-03-10 08:58:23 +0100

    About me content

--- main.js
+++ main.js
@@ -1,4 +1,18 @@
-import './style.css'
+import './style.css';
+import "@ui5/webcomponents/dist/Button.js";
+import "@ui5/webcomponents/dist/TabContainer.js";
+import "@ui5/webcomponents/dist/Tab.js";
+import "@ui5/webcomponents/dist/TabSeparator.js";
+
+import "@ui5/webcomponents-fiori/dist/Bar.js";
+import "@ui5/webcomponents/dist/Panel.js";
+import "@ui5/webcomponents/dist/Card";
+import "@ui5/webcomponents/dist/CardHeader.js";
+import "@ui5/webcomponents-fiori/dist/Timeline.js";
+
+//Icons
+import "@ui5/webcomponents-icons/dist/AllIcons.js";
+
 import { setupCounter } from './counter.js'
 
 // document.querySelector('#app').innerHTML = `
